a35d025162804644b9fb571a7fad66c5eeb421e2
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2021-08-06 08:09:04 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,17 @@ val ktorVersion: String = "1.6.2"
 plugins {
     java
     id("org.jetbrains.kotlin.jvm") version "1.5.30-M1"
-    `maven-publish`
+    id("maven-publish")
 }
 
-group = "com.github.MatthiasReumann"
-version = "0.1.0"
+group = "com.github.matthiasreumann"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
     testImplementation(kotlin("test"))
 
     implementation("io.ktor:ktor-client-core:$ktorVersion")
@@ -23,34 +21,20 @@ dependencies {
     implementation("io.ktor:ktor-client-gson:$ktorVersion")
 }
 
-tasks {
+tasks{
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
-}
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
 
-
-val sourcesJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
-}
-
-
-publishing {
-    publications {
-        create<MavenPublication>("googlebooksapi") {
-            artifacts {
-                archives(sourcesJar)
-            }
-            pom {
-                packaging = "jar"
-                name.set("googlebooksapi")
-                description.set("A Kotlin Wrapper of the Google Books APIs")
-                url.set("https://github.com/MatthiasReumann/googlebooksapi")
-            }
-        }
+    artifacts{
+        archives(sourcesJar)
+        archives(jar)
     }
 }
